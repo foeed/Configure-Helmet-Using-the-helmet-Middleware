34d725ccf0667eb9a078712486befbae02f379f4
foeed <59224767+foeed@users.noreply.github.com>
2022-11-06 16:33:21 +0100

    Update myapp.js

--- myapp.js
+++ myapp.js
@@ -1,12 +1,15 @@
-app.use(helmet({
-  frameguard: {         // configure
-    action: 'deny'
-  },
-  contentSecurityPolicy: {    // enable and configure
-    directives: {
-      defaultSrc: ["'self'"],
-      styleSrc: ['style.com'],
-    }
-  },
-  dnsPrefetchControl: false     // disable
-}))
+const helmet = require('helmet')
+var ninetyDaysInSeconds = 90*24*60*60; 
+
+ 
+app.use(helmet.hidePoweredBy()); 
+app.use(helmet.frameguard({action: 'deny'}));
+ app.use(helmet.xssFilter({}));
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
+//HTTP Strict Transport Security 
+app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
+//To improve performance
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
+app.use(helmet.contentSecurityPolicy({ directives: { defaultSrc: ["'self'"], scriptSrc: ["'self'", "trusted-cdn.com"] }} ));
